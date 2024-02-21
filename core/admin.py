b8eb9f32f58db344a44acb3de4e16e52b2e1c673
Douglous Sobei <douglousmangoyi@gmail.com>
2024-02-21 08:06:29 +0300

    Transaction model using django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from core.models import Transaction
 
-# Register your models here.
+admin.site.register(Transaction)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from userauths.models import User
+from account.models import Account
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+TRANSACTION_TYPE = (
+    ("transfer", "Transfer"),
+    ("recieved", "Recieved"),
+    ("withdraw", "withdraw"),
+    ("refund", "Refund"),
+    ("request", "Payment Request"),
+    ("none", "None")
+)
+
+TRANSACTION_STATUS = (
+    ("failed", "failed"),
+    ("completed", "completed"),
+    ("pending", "pending"),
+    ("processing", "processing"),
+    ("request_sent", "request_sent"),
+    ("request_settled", "request settled"),
+    ("request_processing", "request processing"),
+
+)
+
+
+CARD_TYPE = (
+    ("master", "master"),
+    ("visa", "visa"),
+    ("verve", "verve"),
+
+)
+
+
+NOTIFICATION_TYPE = (
+    ("None", "None"),
+    ("Transfer", "Transfer"),
+    ("Credit Alert", "Credit Alert"),
+    ("Debit Alert", "Debit Alert"),
+    ("Sent Payment Request", "Sent Payment Request"),
+    ("Recieved Payment Request", "Recieved Payment Request"),
+    ("Funded Credit Card", "Funded Credit Card"),
+    ("Withdrew Credit Card Funds", "Withdrew Credit Card Funds"),
+    ("Deleted Credit Card", "Deleted Credit Card"),
+    ("Added Credit Card", "Added Credit Card"),
+
+)
+
+
+class Transaction(models.Model):
+    transaction_id = ShortUUIDField(
+        unique=True, length=15, max_length=20, prefix="TRN")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+
+    reciever = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="reciever")
+    sender = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="sender")
+
+    reciever_account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, null=True, related_name="reciever_account")
+    sender_account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, null=True, related_name="sender_account")
+
+    status = models.CharField(
+        choices=TRANSACTION_STATUS, max_length=100, default="pending")
+    transaction_type = models.CharField(
+        choices=TRANSACTION_TYPE, max_length=100, default="none")
+
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user}"
