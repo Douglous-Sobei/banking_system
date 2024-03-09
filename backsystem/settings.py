279f23f52c6891d251a56a2f3c086c084434c2c2
Douglous Sobei <douglousmangoyi@gmail.com>
2024-03-09 15:59:25 +0300

    update

--- backsystem/settings.py
+++ backsystem/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-ynmq$83b#dkwtnz)x36m!sujtexb@htf&vv#@e3ihd#ql739^v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
